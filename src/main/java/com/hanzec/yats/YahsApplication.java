5c95445224911dd6c5b64d2a972cb9b980239a50
Hanze Chen <chen_hanze@hotmail.com>
2025-02-13 00:56:19 +0800

    Changes

---
+++ src/main/java/com/hanzec/yats/YahsApplication.java
@@ -0,0 +1,13 @@
+package com.hanzec.yats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YahsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YahsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hanzec/yats/YahsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanzec.yats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YahsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
